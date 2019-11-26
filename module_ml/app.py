3a31cae7f43a524d861876cda2861482890f8784
nasrajan <nasrajan.jalin@sjsu.edu>
2019-11-25 19:38:32 -0800

    node js changes

--- module_ml/app.py
+++ module_ml/app.py
@@ -1,16 +1,25 @@
 
 import numpy as np
+import pandas as pd
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 app = Flask(__name__)
+CORS(app)
 model = pickle.load(open('knn.pickle','rb'))
 
+df = pd.read_csv('data.csv')
 
 @app.route('/')
 def hello_world():
     data = [[50, 2, 0.1, 1.0,3.0, 0.0, 0.2, 0.0, 1.0, 50.0, 1.5]]
-    prediction = model.predict(np.asarray(data))
-    return prediction[0]
+    #prediction = model.predict(np.asarray(data))
+    prediction = model.kneighbors(np.asarray(data))
+    vals = prediction[1][0][:10]
+
+    res = df.iloc[vals].to_json(orient='records')
+    #print(prediction)
+    return res
 
 if __name__ == '__main__':
     app.run()
