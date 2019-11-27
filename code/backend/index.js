b407c9a105ff07a1dbddc45427238ac0cd5c5b8a
ayshag <43154634+ayshag@users.noreply.github.com>
2019-11-26 22:43:39 -0800

    database connect & signin signup ui change

--- code/backend/index.js
+++ code/backend/index.js
@@ -8,13 +8,72 @@ app.use(cors({ origin: '*', credentials: true }));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+var database = require('./database');
+
+ 
+
+
+
 app.post('/signup',function (req, res){
-  console.log('signup post request');
   console.log(req.body);
-  const data = {
-    'authenticated' : 'true'
-  }
-  res.end(JSON.stringify(data));
+  database.getConnection(function(error){
+    if(error)
+      {
+        console.log(error);
+        return;
+      }
+      var sql = "INSERT INTO LOGIN VALUES(0,'" +req.body.name+ "', '" + req.body.email + "', '"+ req.body.password +"', true)";
+      console.log(sql);
+      const data = {
+        'authenticated' : ''
+      }
+    
+      database.query(sql, function (error, response) {
+      if (error) 
+      {
+          if(error.code == 'ER_DUP_ENTRY')
+          {
+              data.authenticated = 'emailError';
+          }
+      }
+      else
+      {
+        data.authenticated = 'true';
+      }
+          
+      res.end(JSON.stringify(data));
+      });
+    }
+  )
+  
+});
+
+
+app.post('/signin',function (req, res){
+  console.log(req.body);
+  database.getConnection(function(error){
+    if(error)
+      {
+        console.log(error);
+        return;
+      }
+      var sql = "SELECT EMAIL, PWD FROM LOGIN WHERE EMAIL = '" +req.body.email+ "' AND PWD = '" + req.body.password + "'";
+      console.log(sql);
+      database.query(sql, function (error, response) {
+        if (error) 
+          console.log(error);
+          
+      const data = {
+        'authenticated' : false
+      }
+
+      if(response.length == 1)
+        data.authenticated = true;
+      res.end(JSON.stringify(data));
+      });
+    }
+  )
+  
 });
 
 
