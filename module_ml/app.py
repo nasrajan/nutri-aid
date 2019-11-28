6318ec778e2748e5580a1e5d3db0d692d99fc532
nasrajan <nasrajan.jalin@sjsu.edu>
2019-11-27 22:23:30 -0800

    Added heroku url for Flask

--- module_ml/app.py
+++ module_ml/app.py
@@ -1,5 +1,5 @@
-
 import numpy as np
+import os
 import pandas as pd
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -22,4 +22,6 @@ def hello_world():
     return res
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run()
